48874c7c9373efdc29ff6757a21420381f0ef6e1
marcos <marcosfreire.dev@gmail.com>
2018-12-20 15:11:22 -0300

    listando e exibindo selecionado

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Capitão Caverna'},
+    {id: 12, name: 'Et Bilú'},
+    {id: 13, name: 'Homem Macaco'},
+    {id: 14, name: 'Pai de Família'},
+    {id: 15, name: 'Nazaré'},
+    {id: 16, name: 'Capitão Codorna'},
+    {id: 17, name: 'Frota evangélico'},
+    {id: 18, name: 'Xuxa do Ceará'},
+    {id: 19, name: 'Capitão Caverna'},
+    {id: 20, name: 'Capitão Caverna'}
+];
